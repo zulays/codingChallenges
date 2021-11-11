6317e9d8e028d9ddb400e27ac3c579036b1f0884
zulays <zscottborgh@gmail.com>
2021-11-11 00:38:42 -0500

    create folder for challenge. create solution folder. complete desktop design. complete slider functionality.

---
+++ PNY-Coding-Challenge/README.md
@@ -0,0 +1,34 @@
+# Coding Bootcamp Testimonials Slider
+
+![Design preview for the Coding Bootcamp Testimonials Slider coding challenge](./design/desktop-preview.jpg)
+
+**To do this challenge, you need a basic understanding of HTML, CSS and JavaScript.**
+
+## The challenge
+
+Your challenge is to build out this testimonial slider and get it looking as close to the design as possible.
+
+You can use any tools you like to help you complete the challenge.
+
+Your users should be able to: 
+
+- View the optimal layout for the component depending on their device's screen size
+- Navigate the slider using either their mouse/trackpad or keyboard
+
+
+## Where to find everything
+
+Your task is to build out the project to the designs inside the `/design` folder. You will find both a mobile and a desktop version of the design to work to. 
+
+The designs are in JPG static format. This will mean that you'll need to use your best judgment for styles such as `font-size`, `padding` and `margin`. 
+
+You will find all the required assets in the `/images` folder. The assets are already optimized.
+
+There is also a `style-guide.md` file, which contains the information you'll need, such as color palette and fonts.
+
+
+
+
+
+
+
---
+++ PNY-Coding-Challenge/solution/app.js
@@ -0,0 +1,37 @@
+let user1 = document.querySelector('.user-1');
+let user2 = document.querySelector('.user-2');
+let slides = document.querySelectorAll('.slide');
+let text = document.querySelector('.review-text');
+let userName = document.querySelector('.name');
+let role = document.querySelector('.title');
+let image = document.querySelector('.user-image');
+let prevButton = document.querySelector('.prev');
+let nextButton = document.querySelector('.next');
+
+const userDetails = [
+  {
+    name: 'Tanya Sinclair',
+    title: 'UX Engineer',
+    photo: 'PNY-Coding-Challenge/images/image-tanya.jpg',
+    review: `"I've been interested in coding for a while but never taken the jump, until now. I couldn't recommend this course enough. I'm now in the job of my dreams and so excited about the future."`,
+  },
+  {
+    name: 'John Tarkpor',
+    title: 'Junior Front-end Developer',
+    photo: 'PNY-Coding-Challenge/images/image-john.jpg',
+    review: `"If you want to lay the best foundation possible I'd recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer."`,
+  },
+];
+
+function slideShow() {
+  user1.style.display = 'none';
+  user2.style.display = 'flex';
+}
+
+function slideHide() {
+  user1.style.display = 'flex';
+  user2.style.display = 'none';
+}
+
+prevButton.addEventListener('click', slideHide);
+nextButton.addEventListener('click', slideShow);
