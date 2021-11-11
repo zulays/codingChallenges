4732ec691109a19279e5b624ceb0806c4eca9833
zulays <zscottborgh@gmail.com>
2021-11-11 00:47:36 -0500

    adjust relative image paths to git links

--- PNY-Coding-Challenge/solution/app.js
+++ PNY-Coding-Challenge/solution/app.js
@@ -12,13 +12,15 @@ const userDetails = [
   {
     name: 'Tanya Sinclair',
     title: 'UX Engineer',
-    photo: 'PNY-Coding-Challenge/images/image-tanya.jpg',
+    photo:
+      'https://github.com/zulays/codingChallenges/blob/main/PNY-Coding-Challenge/images/image-tanya.jpg',
     review: `"I've been interested in coding for a while but never taken the jump, until now. I couldn't recommend this course enough. I'm now in the job of my dreams and so excited about the future."`,
   },
   {
     name: 'John Tarkpor',
     title: 'Junior Front-end Developer',
-    photo: 'PNY-Coding-Challenge/images/image-john.jpg',
+    photo:
+      'https://github.com/zulays/codingChallenges/blob/main/PNY-Coding-Challenge/images/image-john.jpg',
     review: `"If you want to lay the best foundation possible I'd recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer."`,
   },
 ];
